9f770ca810cb5f4a5079df7cc892ae171ca1ab3a
Dimitar Janevski <dimitar.j27@gmail.com>
2022-06-03 19:40:38 +0200

    Finish contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,14 +3,18 @@ import { styled } from '@mui/system';
 import { Button, Grid, Typography } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import { useState } from 'react';
+import MediaQuery from 'react-responsive'
 
 import Character from '../assets/Saly-2.png'
 import CharacterV2 from '../assets/Saly-2-cropped.png'
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import CancelIcon from '@mui/icons-material/Cancel';
 
 const Container = styled('div')(({theme}) => ({
     paddingLeft: "10%",
     paddingRight:"10%",
-    marginBottom:"75px",
+    marginBottom:"35px",
     position: "relative",
 }))
 
@@ -29,8 +33,129 @@ const ImageContainer = styled('div')(({theme}) => ({
     }
 }))
 
+const SuccessContainer = styled('div')(({theme}) => ({
+    textAlign: "center",
+    marginTop:"10px",
+    color:"rgb(0,170,83)",
+    display:"flex",
+    gap:"5px",
+    justifyContent:"center",
+    alignItems:"flex-end"
+}))
+
+const ErrorContainer = styled('div')(({theme}) => ({
+    textAlign: "center",
+    marginTop:"10px",
+    color:"#b00020",
+    display:"flex",
+    gap:"5px",
+    justifyContent:"center",
+    alignItems:"flex-end"
+}))
+
 
 export default function Contact() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+    const [submitting, setSubmitting] = useState(false)
+    const [success, setSuccess] = useState(false)
+    const [fail, setFail] = useState(true)
+    const [errorMessage, setErrorMessage] = useState("Email could not be sent. Please try again later, or email me directly at dimitar.j27@gmail.com.")
+    
+    const axios = require('axios').default;
+
+    const handleFieldChange = (event, property) => {
+        if (property === "name"){
+            setName(event.target.value);
+        }
+        else if (property === "email"){
+            setEmail(event.target.value);
+        }
+        else if (property === "message"){
+            setMessage(event.target.value);
+        }
+    }
+
+    const renderSuccessDialog = () => {
+        return (
+            <SuccessContainer>
+                <MediaQuery minWidth={900}>
+                    <CheckCircleIcon></CheckCircleIcon>
+                </MediaQuery>
+                <Typography sx={{
+                    fontSize:"14px",
+                    fontWeight:"700",
+                }}>
+                    Email Sent!
+                </Typography>
+            </SuccessContainer>
+        )
+    }
+
+    const renderErrorDialog = () => {
+        return (
+            <ErrorContainer>
+                <MediaQuery minWidth={900}>
+                    <CancelIcon></CancelIcon>
+                </MediaQuery>
+                <Typography sx={{
+                    fontSize:"14px",
+                    fontWeight:"700",
+                }}>
+                    {errorMessage}
+                </Typography>
+            </ErrorContainer>
+        )
+    }
+
+    function validEmail(mail) 
+    {
+     if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail))
+      {
+        return (true)
+      }
+        return (false)
+    }
+
+    const handleSubmit = () => {
+        if (name === "" || email === "" || message === ""){
+            setErrorMessage("Please fill out all fields before submitting.")
+            setSuccess(false)
+            setFail(true)
+        } else if (!validEmail(email)){
+            setErrorMessage("Please enter a valid email.")
+            setSuccess(false)
+            setFail(true)
+        } else {
+            setSubmitting(true)
+            axios.post('https://41l9b8a1xf.execute-api.us-east-1.amazonaws.com/default/helloWorld', {
+                name: name,
+                email: email,
+                message: message
+              }, {
+                  headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                  }
+              })
+              .then(function (response) {
+                setName("")
+                setEmail("")
+                setMessage("")
+                setSuccess(true)
+                setFail(false)
+                setErrorMessage("")
+              })
+              .catch(function (error) {
+                  setSuccess(false)
+                  setFail(true)
+                  setErrorMessage("Email could not be sent. Please try again later, or email me directly at dimitar.j27@gmail.com.")
+              });
+            console.log("finished")
+            setSubmitting(false)
+        }
+    }
+
     return (
         <Container>
             <Typography variant="h1" style={{color:"#fafafa"}}>
@@ -65,6 +190,8 @@ export default function Contact() {
                         variant="filled" 
                         placeholder="Name"
                         InputProps={{ disableUnderline: true }}
+                        onChange={(e) => {handleFieldChange(e, "name")}}
+                        value={name}
                         sx={{
                             input: {
                                 color:"#fafafa",
@@ -90,7 +217,9 @@ export default function Contact() {
                             fullWidth 
                             variant="filled" 
                             placeholder="Email"
+                            value={email}
                             InputProps={{ disableUnderline: true }}
+                            onChange={(e) => {handleFieldChange(e, "email")}}
                             sx={{
                                 input: {
                                     color:"#fafafa",
@@ -110,7 +239,9 @@ export default function Contact() {
                             fullWidth 
                             variant="filled" 
                             placeholder="Your Message..."
+                            value={message}
                             InputProps={{ disableUnderline: true, style:{height:"200px", alignItems:"flex-start", paddingLeft:"25px", paddingRight:"25px"} }}
+                            onChange={(e) => {handleFieldChange(e, "message")}}
                             sx={{
                                 backgroundColor:"#282828",
                                 borderRadius:"15px",
@@ -144,9 +275,13 @@ export default function Contact() {
                     marginTop:"20px"
                 }}
                 endIcon={<MailOutlineIcon></MailOutlineIcon>}
+                onClick={handleSubmit}
+                disabled={submitting}
             >
                 Send Email
             </Button>
+            {success && renderSuccessDialog()}
+            {fail && renderErrorDialog()}
         </Container>
     )
 }
