713146ee1ad03c6169f73ed6e4540af7a98ae0b8
Dimitar Janevski <dimitar.j27@gmail.com>
2022-12-25 13:34:06 -0700

    add Equa + update background desc

--- src/components/Background.js
+++ src/components/Background.js
@@ -1,45 +1,53 @@
-import React from 'react'
-import { styled } from '@mui/system';
-import { Typography } from '@mui/material';
-import Globe from '../assets/globe.png'
+import React from "react";
+import { styled } from "@mui/system";
+import { Typography } from "@mui/material";
+import Globe from "../assets/globe.png";
 
-const Container = styled('div')(({ theme }) => ({
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-    margin: "10% 10% 5% 10%",
-    gap: "80px",
-    [theme.breakpoints.down("md")]: {
-        flexDirection: "column",
-        gap: "40px"
-    }
-}));
-
-const TextContainer = styled('div')(({themse}) => ({
-    display: "flex",
+const Container = styled("div")(({ theme }) => ({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  margin: "10% 10% 5% 10%",
+  gap: "80px",
+  [theme.breakpoints.down("md")]: {
     flexDirection: "column",
     gap: "40px",
+  },
 }));
 
-const Description = styled('Typography')(({theme}) => ({
-    fontSize: "clamp(1.20rem, 1vw + 0.65rem, 1.40rem)",
-    fontWeight: 300,
-}))
+const TextContainer = styled("div")(({ themse }) => ({
+  display: "flex",
+  flexDirection: "column",
+  gap: "40px",
+}));
+
+const Description = styled("Typography")(({ theme }) => ({
+  fontSize: "clamp(1.20rem, 1vw + 0.65rem, 1.40rem)",
+  fontWeight: 300,
+}));
 
 export default function Background() {
-    return (
-        <section style={{position: "relative"}}>
-            <Container>
-                <TextContainer>
-                    <Typography variant="h1" style={{color:"#fafafa"}}>
-                        Background
-                    </Typography>
-                    <Description style={{color:"#fafafa"}}>
-                        I was born and raised in a small country in Europe called Macedonia 🇲🇰 I moved to Calgary in 2009 and have been living here since then! I am in my 3rd year of Software Engineering at the University of Calgary, and am currently working as a Full Stack Engineering Intern at a startup, FansFirst. In my free time, I enjoy building projects and learning new things 👨‍💻 playing soccer ⚽ snowboarding 🏂 and traveling ✈️
-                    </Description>
-                </TextContainer>
-                <img src={Globe} width="100%"></img>
-            </Container>
-        </section>
-    )
+  return (
+    <section style={{ position: "relative" }}>
+      <Container>
+        <TextContainer>
+          <Typography variant="h1" style={{ color: "#fafafa" }}>
+            Background
+          </Typography>
+          <Description style={{ color: "#fafafa" }}>
+            I was born and raised in a small country in Europe called Macedonia
+            🇲🇰 I moved to Calgary in 2009 and have been living here since then!
+            I am in my 3rd year of Software Engineering at the University of
+            Calgary, and am currently working as a Full Stack Engineering Intern
+            at a startup, FansFirst. I'll be in San Francisco for Summer 2023
+            interning with r2c on the Application Development team and I am
+            looking for Fall 2023 and Winter 2024 internships. In my free time,
+            I enjoy building projects and learning new things 👨‍💻 playing soccer
+            ⚽ snowboarding 🏂 and traveling ✈️
+          </Description>
+        </TextContainer>
+        <img src={Globe} width="100%"></img>
+      </Container>
+    </section>
+  );
 }
--- src/constants/project_data.js
+++ src/constants/project_data.js
@@ -1,95 +1,117 @@
-import Github from '../assets/githubWhite.png'
-import Website from '../assets/websiteWhite.png'
-import YouTube from '../assets/youtubeWhite.png'
-import DevPost from '../assets/devpostWhite.png'
+import Github from "../assets/githubWhite.png";
+import Website from "../assets/websiteWhite.png";
+import YouTube from "../assets/youtubeWhite.png";
+import DevPost from "../assets/devpostWhite.png";
 
 const content = [
-    {
-        name: "RingRoad",
-        description: "Developed an <b>endless arcade mobile game</b>, RingRoad, for <i>iOS</i> and <i>Android</i> using <b>C#</b> and the <b>Unity Game Engine</b>. Designed 3D levels and game assets using <b>Blender</b>. Implemented daily rewards, Unity In-App Purchases, and Unity Ads. Currently available only on the Google Play Store.",
-        links: [
-            {
-                url: "https://play.google.com/store/apps/details?id=com.DJGames.RingTheRoad",
-                icon: Website
-            },
-            {
-                url: "https://youtu.be/lH9U9Jh0O8g",
-                icon: YouTube
-            }
-        ]
-    },
-    {
-        name: "MyChef",
-        description: "Designed and developed a web-app connecting students with local home cooks in order to provide a <b>healthier alternative to current take-out and food delivery options</b>. Designed site with <b>Figma</b>, and implemented using <b>React</b> and a <b>Firebase</b> backend. Built in a group of 5 during a 24 hour hackathon with 500+ participants and received the <b>Most Innovative Idea Award</b>.",
-        links: [
-            {
-                url: "https://github.com/dimitar-j/Calgary_Hacks_2022",
-                icon: Github
-            },
-            {
-                url: "https://devpost.com/software/mychef-yo1d4j",
-                icon: DevPost
-            }
-        ]
-    },
-    {
-        name: "DuoChef",
-        description: "Designed and developed a <b>gamified way to learn cooking</b> with curated recipes, daily streaks, and a social feed and leaderboard. Utilizes computer vision and machine learning to award points based on successful completion of recipe. Built with <b>React/Firebase</b> in 24 hours with a group of 4 as part of Hack Your Learning 2022. Received <b>1st place</b> amongst 60+ participants.",
-        links: [
-            {
-                url: "https://github.com/dimitar-j/Hack-Your-Learning-2022",
-                icon: Github
-            },
-            {
-                url: "https://devpost.com/software/duochef",
-                icon: DevPost
-            }
-        ]
-    },
-    {
-        name: "Food Bank System",
-        description: "In a group of 4 developed a <b>Java</b> app using <b>SQL</b> and <b>SwingUI</b> to allow a food bank to create optimized food hampers based on current food available and needs of families in the order. Created a <b>UML diagram</b> during design stage and implemented <b>unit tests</b> using <b>Junit</b>.",
-        links: [
-            {
-                url: "https://github.com/dimitar-j/ENSF409-Project",
-                icon: Github
-            },
-        ]
-    },
-    {
-        name: "Weather Analyzer",
-        description: "Weather analyzer written in <b>Python</b>. Utilizes <b>NumPy</b> to analyze Calgary weather data over 20 years from a CSV file. Visualizes data to provide user a visual summary of data anlysis using <b>Pyplot</b>.",
-        links: [
-            {
-                url: "https://github.com/dimitar-j/Weather-Analyzer",
-                icon: Github
-            },
-        ]
-    },
-    {
-        name: "Personal Site",
-        description: "Designed personal site showcasing my background and experience using <b>Figma</b>. Implemented using <b>React</b> and <b>MaterialUI</b>, and built a serverless <b>AWS Lambda API</b> to allow users to contact me by sendeing me an email directly from the site.",
-        links: [
-            {
-                url: "https://www.google.com",
-                icon: Website
-            },
-            {
-                url: "https://github.com/dimitar-j/personal-site",
-                icon: Github
-            },
-        ]
-    },
-    {
-        name: "AI Tic-Tac-Toe",
-        description: "Built an AI written in <b>Python</b> which utilizes the Minimax algorithm to play TicTacToe intelligently against the user.",
-        links: [
-            {
-                url: "https://github.com/dimitar-j/ai-tictactoe",
-                icon: Github
-            },
-        ]
-    },
-]
+  {
+    name: "RingRoad",
+    description:
+      "Developed an <b>endless arcade mobile game</b>, RingRoad, for <i>iOS</i> and <i>Android</i> using <b>C#</b> and the <b>Unity Game Engine</b>. Designed 3D levels and game assets using <b>Blender</b>. Implemented daily rewards, Unity In-App Purchases, and Unity Ads. Currently available only on the Google Play Store.",
+    links: [
+      {
+        url: "https://play.google.com/store/apps/details?id=com.DJGames.RingTheRoad",
+        icon: Website,
+      },
+      {
+        url: "https://youtu.be/lH9U9Jh0O8g",
+        icon: YouTube,
+      },
+    ],
+  },
+  {
+    name: "MyChef",
+    description:
+      "Designed and developed a web-app connecting students with local home cooks in order to provide a <b>healthier alternative to current take-out and food delivery options</b>. Designed site with <b>Figma</b>, and implemented using <b>React</b> and a <b>Firebase</b> backend. Built in a group of 5 during a 24 hour hackathon with 500+ participants and received the <b>Most Innovative Idea Award</b>.",
+    links: [
+      {
+        url: "https://github.com/dimitar-j/Calgary_Hacks_2022",
+        icon: Github,
+      },
+      {
+        url: "https://devpost.com/software/mychef-yo1d4j",
+        icon: DevPost,
+      },
+    ],
+  },
+  {
+    name: "DuoChef",
+    description:
+      "Designed and developed a <b>gamified way to learn cooking</b> with curated recipes, daily streaks, and a social feed and leaderboard. Utilizes computer vision and machine learning to award points based on successful completion of recipe. Built with <b>React/Firebase</b> in 24 hours with a group of 4 as part of Hack Your Learning 2022. Received <b>1st place</b> amongst 60+ participants.",
+    links: [
+      {
+        url: "https://github.com/dimitar-j/Hack-Your-Learning-2022",
+        icon: Github,
+      },
+      {
+        url: "https://devpost.com/software/duochef",
+        icon: DevPost,
+      },
+    ],
+  },
+  {
+    name: "Food Bank System",
+    description:
+      "In a group of 4 developed a <b>Java</b> app using <b>SQL</b> and <b>SwingUI</b> to allow a food bank to create optimized food hampers based on current food available and needs of families in the order. Created a <b>UML diagram</b> during design stage and implemented <b>unit tests</b> using <b>Junit</b>.",
+    links: [
+      {
+        url: "https://github.com/dimitar-j/ENSF409-Project",
+        icon: Github,
+      },
+    ],
+  },
+  {
+    name: "Weather Analyzer",
+    description:
+      "Weather analyzer written in <b>Python</b>. Utilizes <b>NumPy</b> to analyze Calgary weather data over 20 years from a CSV file. Visualizes data to provide user a visual summary of data anlysis using <b>Pyplot</b>.",
+    links: [
+      {
+        url: "https://github.com/dimitar-j/Weather-Analyzer",
+        icon: Github,
+      },
+    ],
+  },
+  {
+    name: "Personal Site",
+    description:
+      "Designed personal site showcasing my background and experience using <b>Figma</b>. Implemented using <b>React</b> and <b>MaterialUI</b>, and built a serverless <b>AWS Lambda API</b> to allow users to contact me by sendeing me an email directly from the site.",
+    links: [
+      {
+        url: "https://www.google.com",
+        icon: Website,
+      },
+      {
+        url: "https://github.com/dimitar-j/personal-site",
+        icon: Github,
+      },
+    ],
+  },
+  {
+    name: "AI Tic-Tac-Toe",
+    description:
+      "Built an AI written in <b>Python</b> which utilizes the Minimax algorithm to play TicTacToe intelligently against the user.",
+    links: [
+      {
+        url: "https://github.com/dimitar-j/ai-tictactoe",
+        icon: Github,
+      },
+    ],
+  },
+  {
+    name: "Equa",
+    description:
+      "Designed and developed Equa, a chrome extension and web app which <b>incentivizes users to donate</b> to causes they are passionate about and <b>make charitable donations accessible and easy for everyone</b>. Personally designed extension and web app using <b>Figma</b>, built the chrome extension using <b>Plasmo</b>, and assisted in building <b>React</b> web app.",
+    links: [
+      {
+        url: "https://github.com/brian-ngyn/Hack-The-Change-2022",
+        icon: Github,
+      },
+      {
+        url: "https://devpost.com/software/equa",
+        icon: DevPost,
+      },
+    ],
+  },
+];
 
 export default content;
